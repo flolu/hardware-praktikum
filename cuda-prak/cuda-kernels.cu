732632e561dea68b432922ce3174ac8e902ed3cb
flolu <loflude@gmail.com>
2021-04-21 20:42:11 +0200

    mirrorKernel

--- cuda-prak/cuda-kernels.cu
+++ cuda-prak/cuda-kernels.cu
@@ -50,7 +50,38 @@ __global__ void linearTransformKernel(unsigned char* img_in, unsigned char* img_
 
 __global__ void mirrorKernel(unsigned char* img_in, unsigned char* img_out, int width, int height)
 {
-   //TODO: Aufgabe 2.3 Spiegeln
+  int i = threadIdx.x+blockIdx.x*blockDim.x;
+  int j = threadIdx.y+blockIdx.y*blockDim.y;
+
+  if (i<width && j<height)
+  {
+    int adrIn=(i+j*width)*4;
+    int adrOut=adrIn;
+    unsigned char r,g,b,a;
+    r = img_in[adrIn+0];
+    g = img_in[adrIn+1];
+    b = img_in[adrIn+2];
+    a = img_in[adrIn+3];
+
+    int adrMirrored = ((width - i) + j * width) * 4;
+
+    float r_new = r;
+    float g_new = g;
+    float b_new = b;
+    float a_new = a;
+
+    if (i >= width/2) {
+      r_new = img_in[adrMirrored+0];
+      g_new = img_in[adrMirrored+1];
+      b_new = img_in[adrMirrored+2];
+      a_new = img_in[adrMirrored+3];
+    }
+
+    img_out[adrOut+0] = r_new;
+    img_out[adrOut+1] = g_new;
+    img_out[adrOut+2] = b_new;
+    img_out[adrOut+3] = a_new;
+  }
 }
 
 __global__ void bwKernel(unsigned char* img_in, unsigned char* img_out, int width, int height)
